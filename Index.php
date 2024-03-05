065194a6895601e869aaaac6a3703ab639245143
p.veltman <p.veltman@graafschapcollege.nl>
2024-03-05 18:24:43 +0100

    first commit

---
+++ Index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+</head>
+<body>
+    <h1><?php echo "Hello World" ?></h1>
+    <?php ini_set('display_errors', 'On');
+          error_reporting(E_ALL);
+    ?>
+</body>
+</html>
