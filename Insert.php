9e737f4ca3aea157f42d6276249b612d94ec981e
bramkoskamp <bramkoskamp@student.graafschapcollege.nl>
2024-03-12 13:19:59 +0100

    file_change

--- Insert.php
+++ Insert.php
@@ -6,39 +6,37 @@
     <title>Document</title>
 </head>
 <body>
-    <?php       
-        ini_set('display_errors', 'On');
-        error_reporting(E_ALL);
+    <?php
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
 
-        $host     = 'localhost';
-        $db       = 'net24bkoskamp_db_netwerken';
-        $user     = 'net24bkoskamp_bram';
-        $password = 'Welkom123!';
-        $port     = 2083;
-        $charset  = 'utf8mb4';
-          
-        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    $host = 'gebruikersnaam.gc-webhosting.nl';
+    $dbname = 'database_name';
+    $user = 'gebruikersnaam';
+    $password = 'your_password';
+    $port = '3306';
+    $charset = 'utf8mb4';
 
-        $db = new mysqli($host, $user, $password, $db, $port);
+    $dsn = "mysql:host=$host;dbname=$dbname;port=$port;charset=$charset";
 
-        $db->set_charset($charset);
-        $db->options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
-    
-        $first_name =  $_REQUEST['fname'];
+    try {
+        $pdo = new PDO($dsn, $user, $password);
+        // Set PDO to throw exceptions on errors
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // Set the server timezone to UTC
+        $pdo->exec("SET time_zone = '+00:00'");
+
+        $first_name = $_REQUEST['fname'];
         $last_name = $_REQUEST['lname'];
-        $date_of_birth =  $_REQUEST['bdate'];
+        $date_of_birth = $_REQUEST['bdate'];
 
-        $sql = "INSERT INTO users (first_name, last_name, Date_Of_Birth) VALUES ('$first_name', 
-        '$last_name','$date_of_birth')";
+        $stmt = $pdo->prepare("INSERT INTO users (first_name, last_name, Date_Of_Birth) VALUES (?, ?, ?)");
+        $stmt->execute([$first_name, $last_name, $date_of_birth]);
 
-    if(mysqli_query($db, $sql)){
-        echo "<h3>User added</h3>"; 
-
-        echo nl2br("\n$first_name $last_name\n "
-            . "$date_of_birth");
-    } else{
-        echo "ERROR: Sorry $sql. "
-            . mysqli_error($db);
+        echo "<h3>User added</h3>";
+        echo nl2br("\n$first_name $last_name\n" . "$date_of_birth");
+    } catch (PDOException $e) {
+        echo "ERROR: Sorry " . $e->getMessage();
     }
     ?>
 </body>
